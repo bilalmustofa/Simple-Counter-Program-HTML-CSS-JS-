871ee7f63c157c03ed1eb9c2a1e5c57f5200c52e
bilal <bilalmustofa146@gmail.com>
2026-04-28 12:45:33 -0600

    add function to save the dark mode

--- script.js
+++ script.js
@@ -7,34 +7,45 @@ let changeBy = document.getElementById("changeBy");
 let count = 0;
 
 // Increasing function
-increaseBtn.onclick = function() {
-    const changeByValue = parseInt(changeBy.value); // change string in to number
-    count = count + changeByValue;
-    counter.textContent = count;
-}
+increaseBtn.onclick = function () {
+  const changeByValue = parseInt(changeBy.value); // change string in to number
+  count = count + changeByValue;
+  counter.textContent = count;
+};
 
 // Reset function
-resetBtn.onclick = function() {
-    count = 0;
-    counter.textContent = count;
-}
+resetBtn.onclick = function () {
+  count = 0;
+  counter.textContent = count;
+};
 
 // Decreasing function
-decreaseBtn.onclick = function() {
-    const changeByValue = parseInt(changeBy.value); // change string in to number 
-    count = count - changeByValue;
-    counter.textContent = count;
-}
+decreaseBtn.onclick = function () {
+  const changeByValue = parseInt(changeBy.value); // change string in to number
+  count = count - changeByValue;
+  counter.textContent = count;
+};
 
 // Dark Mode
 const darkModeCheckbox = document.getElementById("darkModeCheckbox");
 darkModeCheckbox.addEventListener("change", () => {
-    if(darkModeCheckbox.checked){
-        document.body.style.backgroundColor = "rgb(39, 41, 45)";
-        document.getElementById("counterBox").classList.add("dark");
-    } else {
-        document.body.style.backgroundColor = "";
-        document.getElementById("counterBox").classList.remove("dark");
-    }
+  if (darkModeCheckbox.checked) {
+    document.body.classList.add("dark");
+    document.getElementById("counterBox").classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark");
+    document.getElementById("counterBox").classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  }
 });
 
+// Load saved mode
+window.onload = function () {
+  let theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    document.body.classList.add("dark");
+    document.getElementById("counterBox").classList.add("dark");
+    darkModeCheckbox.checked = true;
+  }
+};
